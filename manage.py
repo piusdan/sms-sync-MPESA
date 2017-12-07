048d5e3cb4ea31715f395677048ce4f8b338f857
Pius Dan Nyongesa <npiusdan@gmail.com>
2017-12-07 10:31:55 +0300

    Initial Commit

---
+++ manage.py
@@ -0,0 +1,20 @@
+import os
+
+from flask_migrate import Migrate
+from flask_socketio import SocketIO
+
+from app import create_app
+from app.database import db
+from app.models import Topic, Feedback, Screen
+
+socketio = SocketIO()
+
+config_name = os.environ.get('FLASK_CONFIG') or 'default'
+app = create_app(config_name)
+migrate = Migrate(app, db)
+socketio.init_app(app)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, Topic=Topic, Screen=Screen, Feedback=Feedback)

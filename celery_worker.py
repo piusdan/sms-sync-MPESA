3383da420c261a99bbbf0e25a08ab1e66673d445
Pius Dan Nyongesa <npiusdan@gmail.com>
2017-12-11 11:40:40 +0300

    FIX: Celery bug

--- celery_worker.py
+++ celery_worker.py
@@ -1,6 +1,9 @@
 import os
 
 from app import create_app
+from app.celery_cfg import celery
 
 app = create_app(os.getenv('USSD_CONFIG') or 'default')
 app.app_context().push()
+
+celery.conf.update(app.config)
